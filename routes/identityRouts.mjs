56917c188deb4824980ab6b3078dfac3f8f25e0b
Usman Ghani <usman.dev24@gmail.com>
2025-10-07 12:35:26 +0500

    fix : due to change app url storing ids is useLess

--- routes/identityRouts.mjs
+++ routes/identityRouts.mjs
@@ -6,35 +6,11 @@ import * as fs from "node:fs/promises";
 
 
 addRoute("/set-device-name", async (req, res) => {
-  await varifyDir("appData");
-  await varifyFile("appData", "devicesData.json");
-
   const reqDeviceName = req.headers.devicename;
   const cookie = cookieParser(req.headers.cookie);
+  res.setHeader("set-cookie", `devicename=${reqDeviceName}; HttpOnly; Path=/; Max-Age=${60 * 60 * 24 * 365}`);
+  res.end("done");
 
-  const filePath = path.join("appData", "devicesData.json");
-  const readed = await fs.readFile(filePath, "utf-8");
-  let devicesData = readed ? JSON.parse(readed) : Object.create(null);
-
-  const existingId = Object.keys(devicesData).find(
-    id => devicesData[id].name === reqDeviceName
-  );
-
-  if (existingId) {
-    if (existingId === cookie.deviceid) {
-      // Same device is updating its own name
-      devicesData[cookie.deviceid].name = reqDeviceName;
-      res.setHeader("set-cookie", `devicename=${reqDeviceName}; HttpOnly; Path=/; Max-Age=${60 * 60 * 24 * 365}`);
-      res.end("done");
-    } else {
-      res.end("already present");
-    }
-  } else {
-    devicesData[cookie.deviceid] = { name: reqDeviceName };
-    res.setHeader("set-cookie", `devicename=${reqDeviceName}; HttpOnly; Path=/; Max-Age=${60 * 60 * 24 * 365}`);
-    res.end("done");
-  }
-  await fs.writeFile(filePath, JSON.stringify(devicesData), "utf-8");
 });
 
 addRoute("/clear-cookie", (req, res) => {
